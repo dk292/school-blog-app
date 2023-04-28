8649261a39c2608d61f287b181300016f5cc1a35
Htet Hlaing Win <htethlaingwin.bdm29@gmail.com>
2023-04-28 09:48:52 +0630

    Set Up firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'school-blog-9d820',
+    appId: '1:443431655492:web:308c99191bc34dcc3c4411',
+    storageBucket: 'school-blog-9d820.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAkmLdi5nsdKW_clqUDZcL3amdcet8H9ZY',
+    authDomain: 'school-blog-9d820.firebaseapp.com',
+    messagingSenderId: '443431655492',
+  },
+      production: false,
+  
+  }
